fefe1c41355cd893b81a6f89c56672c5a3709ee4
MarkHG <markhg@ciencias.unam.mx>
2021-01-12 17:36:04 -0600

    Sesión 1: PostWork

---
+++ Módulo 2/Sesion1/postwork.R
@@ -0,0 +1,43 @@
+getwd()
+
+# Fijando el directorio de trabajo
+
+setwd("/home/marco/Documentos/BEDU/modulo2/sesion1/postwork") # Depende del usuario
+
+# La función read.csv será util para leer fichero .csv
+# Del data frame que resulta de importar los datos a R, extrae las columnas que contienen los números de goles anotados por los equipos que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
+gol <- read.csv("SP1.csv") # El archivo csv debe estar en el directorio de trabajo
+gol # Variable con los datos de soccer de la temporada 2019/2020 de la primera división de la liga española 
+
+
+local <- gol$FTHG
+visitante <- gol$FTAG
+
+total <- 0
+
+for(i in local) {
+  total <- i + total
+  #print(i)
+}
+print(total)
+
+total.dos <- 0
+
+for(i in visitante) {
+  total.dos <- i + total.dos
+  #print(i)
+}
+print(total.dos)
+
+total.total <- total + total.dos
+#(mifile <- data.frame(local = local, visitante = visitante))
+
+marginal.casa <- (total / total.total)
+marginal.visita <- (total.dos / total.total)
+marginal.conjunta <- marginal.casa * marginal.visita
+
+# Consulta cómo funciona la función table en R al ejecutar en la consola ?table
+?table
+#Prueba de la función table
+barplot(table(marginal.local))
+barplot(table(marginal.visita))
